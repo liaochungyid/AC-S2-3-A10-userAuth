e024c215a8c6a5ba6ec2b29a5f9bba2a45ebb0c2
Liao CY <liaochungyid@gmail.com>
2021-10-03 21:32:30 +0800

    init project

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+
+const users = [
+  {
+    firstName: 'Tony',
+    email: 'tony@stark.com',
+    password: 'iamironman'
+  },
+  {
+    firstName: 'Steve',
+    email: 'captain@hotmail.com',
+    password: 'icandothisallday'
+  },
+  {
+    firstName: 'Peter',
+    email: 'peter@parker.com',
+    password: 'enajyram'
+  },
+  {
+    firstName: 'Natasha',
+    email: 'natasha@gamil.com',
+    password: '*parol#@$!'
+  },
+  {
+    firstName: 'Nick',
+    email: 'nick@shield.com',
+    password: 'password'
+  }
+]
+
+app.get('/', (req, res) => {
+  res.send('Hello world!')
+})
+
+app.listen(3000, () => {
+  console.log('server is listening on http://localhost:300')
+})
